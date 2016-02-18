2d2cb81884e93739ebf06962745ee3ce1c63c8c9
unknown <bideblade1@hotmail.com>
2016-02-18 19:18:30 +0100

    Added some glfw calls.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #define GLFW_INCLUDE_VULKAN
 
-#include <windows.h>
-#include <iostream>
 #include <vulkan/vulkan.h>
 
 #include "vkhelper.h"
@@ -9,19 +7,31 @@
 
 int main() {
 
-	const char* extensionNames[] = { "VK_KHR_surface", "VK_KHR_win32_surface" };
+	WindowHelper::InitGLFW();
 
-	VkInstance inst = VkHelper::CreateVkInstance(extensionNames);
+	int count;
+	const char** extensions = glfwGetRequiredInstanceExtensions(&count);
+
+	PFN_vkCreateInstance pfnCreateInstance = (PFN_vkCreateInstance)
+    glfwGetInstanceProcAddress(NULL, "vkCreateInstance");
+
+	VkInstance inst = VkHelper::CreateVkInstance(extensions);
+
+	PFN_vkCreateDevice pfnCreateDevice = (PFN_vkCreateDevice)
+    glfwGetInstanceProcAddress(inst, "vkCreateDevice");
+
+	PFN_vkGetDeviceProcAddr pfnGetDeviceProcAddr = (PFN_vkGetDeviceProcAddr)
+    glfwGetInstanceProcAddress(inst, "vkGetDeviceProcAddr");
 
 	VkPhysicalDevice phys[4]; uint32_t physCount = 4;
 	vkEnumeratePhysicalDevices(inst, &physCount, phys);
 	VkHelper::LogVkPhysicalDeviceInfo(phys[0]);
 
-	VkDevice dev = VkHelper::CreateVkDevice(phys[0], extensionNames);
+	VkDevice dev = VkHelper::CreateVkDevice(phys[0], extensions);
 
-	WindowHelper::InitGLFW();
 	GLFWwindow* window = WindowHelper::CreateGLFWWindow(400, 320, "Vulkan Learning");
 	VkSurfaceKHR surf = WindowHelper::CreateSurface(inst, window);
+
 
 	WindowHelper::StartLoop(window);
 
