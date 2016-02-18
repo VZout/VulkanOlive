ad222fd7dbf2ad59379c93f394e1476316dc22dd
unknown <bideblade1@hotmail.com>
2016-02-18 19:32:41 +0100

    Added some documentation and removed .atom-build.json from the repo

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,37 @@
 
 int main() {
 
+	// Init GLFW
 	WindowHelper::InitGLFW();
 
 	int count;
-	const char** extensions = glfwGetRequiredInstanceExtensions(&count);
+	const char** extensions = glfwGetRequiredInstanceExtensions(&count); // Use GLFW to get extensions.
 
-	PFN_vkCreateInstance pfnCreateInstance = (PFN_vkCreateInstance)
-    glfwGetInstanceProcAddress(NULL, "vkCreateInstance");
+	PFN_vkCreateInstance pfnCreateInstance = (PFN_vkCreateInstance) // <-- not sure what this does
+    glfwGetInstanceProcAddress(NULL, "vkCreateInstance"); // <-- not sure what this does
 
-	VkInstance inst = VkHelper::CreateVkInstance(extensions);
+	VkInstance inst = VkHelper::CreateVkInstance(extensions); 	// Create VkInstance.
 
-	PFN_vkCreateDevice pfnCreateDevice = (PFN_vkCreateDevice)
-    glfwGetInstanceProcAddress(inst, "vkCreateDevice");
+	PFN_vkCreateDevice pfnCreateDevice = (PFN_vkCreateDevice) // <-- not sure what this does
+    glfwGetInstanceProcAddress(inst, "vkCreateDevice"); // <-- not sure what this does
 
-	PFN_vkGetDeviceProcAddr pfnGetDeviceProcAddr = (PFN_vkGetDeviceProcAddr)
-    glfwGetInstanceProcAddress(inst, "vkGetDeviceProcAddr");
+	PFN_vkGetDeviceProcAddr pfnGetDeviceProcAddr = (PFN_vkGetDeviceProcAddr)// <-- not sure what this does
+    glfwGetInstanceProcAddress(inst, "vkGetDeviceProcAddr"); // <-- not sure what this does
 
 	VkPhysicalDevice phys[4]; uint32_t physCount = 4;
-	vkEnumeratePhysicalDevices(inst, &physCount, phys);
-	VkHelper::LogVkPhysicalDeviceInfo(phys[0]);
+	vkEnumeratePhysicalDevices(inst, &physCount, phys); // Get physical devices.
+	VkHelper::LogVkPhysicalDeviceInfo(phys[0]); // print physical device details.
 
+	// Create VkDevice
 	VkDevice dev = VkHelper::CreateVkDevice(phys[0], extensions);
 
+	//Create GLFW window
 	GLFWwindow* window = WindowHelper::CreateGLFWWindow(400, 320, "Vulkan Learning");
+
+	//Create Vulkan Surface with GLFW
 	VkSurfaceKHR surf = WindowHelper::CreateSurface(inst, window);
 
-
+	// Start a basic while/game loop.
 	WindowHelper::StartLoop(window);
 
 	return 0;
